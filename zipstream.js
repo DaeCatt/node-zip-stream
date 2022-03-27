04141aba9fcc0538913a2c2de0e059ff59ac292d
Dae Catt <1660313+DaeCatt@users.noreply.github.com>
2022-03-27 22:41:02 +0200

    Improve zipstream.js JSDoc

--- zipstream.js
+++ zipstream.js
@@ -5,7 +5,8 @@ const zlib = require("zlib");
 const crc32 = require("./crc32.js");
 
 const FILENAME_RE = /^[^\\?%*:|"<>]+(?:\/[^\\?%*:|"<>]+)*$/;
-const LOCAL_HEADER_COMPRESS_BIT_FLAGS = (1 << 3) | (1 << 11);
+const DEFLATE = 0x08;
+const COMPRESSION_FLAGS = (1 << 3) | (1 << 11);
 
 /**
  * @param {stream.Writable} target
@@ -17,6 +18,23 @@ const promiseWrite = (target, chunk) =>
 		target.write(chunk, (error) => (error ? reject(error) : resolve()));
 	});
 
+/**
+ * Usage
+ * ```
+ * const fs = require("fs");
+ * const ZIPStream = require("node-zip-stream");
+ *
+ * const zip = new ZIPStream();
+ * // Point the zip stream somewhere
+ * zip.pipe(fs.createWriteStream("file.zip"));
+ *
+ * // Add a file, resolves when stream has ended
+ * await zip.addFile("filename.txt", fs.createReadStream("file.txt"));
+ *
+ * // Finish the zip file, resolves when flushed
+ * await zip.flush();
+ * ```
+ */
 class ZIPStream extends stream.Transform {
 	bytes = 0;
 	records = 0;
@@ -29,24 +47,27 @@ class ZIPStream extends stream.Transform {
 
 	/**
 	 * @param {string} filename
-	 * @param {stream.Readable} fileStream
+	 * @param {AsyncIterable<Uint8Array>} fileStream
 	 */
 	async addFile(filename, fileStream) {
 		if (!FILENAME_RE.test(filename)) throw new Error("Invalid filename.");
 		const start = this.bytes;
 		this.records++;
 
+		// Create local file header
 		const filenameBuf = Buffer.from(filename, "utf-8");
 		const lfh = Buffer.alloc(30 + filenameBuf.byteLength);
 		lfh.writeUInt32LE(0x04034b50, 0);
 		lfh.writeUint16LE(20, 4);
-		lfh.writeUint16LE(LOCAL_HEADER_COMPRESS_BIT_FLAGS, 6);
-		lfh.writeUint16LE(8, 8); // DEFLATE
+		// Data descriptor flag is set here since we don't have the original file yet.
+		lfh.writeUint16LE(COMPRESSION_FLAGS, 6);
+		lfh.writeUint16LE(DEFLATE, 8);
 		lfh.writeUint16LE(filenameBuf.byteLength, 26);
 		filenameBuf.copy(lfh, 30);
 
 		await promiseWrite(this, lfh);
 
+		// Determine CRC-32, compressed file size, and original file size
 		let fileCRC32 = 0xffffffff;
 		let compressedBytes = 0;
 		let origBytes = 0;
@@ -70,6 +91,7 @@ class ZIPStream extends stream.Transform {
 
 		await Promise.all([compressorPromise(), filePromise()]);
 
+		// Create data descriptor
 		const dd = Buffer.alloc(12);
 		dd.writeUint32LE(fileCRC32, 0);
 		dd.writeUint32LE(compressedBytes, 4);
@@ -77,11 +99,12 @@ class ZIPStream extends stream.Transform {
 
 		await promiseWrite(this, dd);
 
+		// Create central directory file header
 		const cdfh = Buffer.alloc(46 + filenameBuf.byteLength);
 		cdfh.writeUint32LE(0x02014b50, 0);
 		cdfh.writeUint16LE(20, 6);
-		cdfh.writeUint16LE(LOCAL_HEADER_COMPRESS_BIT_FLAGS, 8);
-		cdfh.writeUint16LE(8, 10); // DEFLATE
+		cdfh.writeUint16LE(COMPRESSION_FLAGS, 8);
+		cdfh.writeUint16LE(DEFLATE, 10);
 		cdfh.writeUint32LE(fileCRC32, 16);
 		cdfh.writeUint32LE(compressedBytes, 20);
 		cdfh.writeUint32LE(origBytes, 24);
@@ -91,6 +114,9 @@ class ZIPStream extends stream.Transform {
 		this.cd = Buffer.concat([this.cd, cdfh]);
 	}
 
+	/**
+	 * Mark end of zip file and flush central directory
+	 */
 	async flush() {
 		const start = this.bytes;
 		await promiseWrite(this, this.cd);
